30c3f89332052e2f28bac2facb0c7e505f15b7e4
Hancel.Lin <imlinhanchao@gmail.com>
2019-11-06 22:35:00 +0800

    更新测试方法

--- test.js
+++ test.js
@@ -6,16 +6,32 @@ const options = {
     username: '',
     repository: ''
 };
-const github = require('.')(options);
 
 if (options.token == '' || options.username == '' || options.repository == '') {
     throw new Error('Please Edit file to config options first.');
 }
 
-test('Upload readme to repository', async function(assert) {
-    assert.deepEqual(await github.upload({
-        data: path.resolve(__dirname, 'readme.md'),
-        filename: 'readme.txt'
-    }), `http://${username}.github.io/${repository}/readme.txt`);
-    assert.end()
-})
+function sleep (time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+(async () => {
+
+    const github = require('.')({
+        token: options.token,
+        repositoryUrl: `https://github.com/${options.username}/${options.repository}`
+        
+    });
+
+    console.log('wait for init.');
+    await sleep(2000);
+    
+    test('Upload readme to repository', async function(assert) {
+        assert.deepEqual(await github.upload({
+            data: path.resolve(__dirname, 'readme.md'),
+            filename: 'readme.txt'
+        }), `https://${options.username}.github.io/${options.repository}/readme.txt`);
+        assert.end()
+    })
+
+})()
